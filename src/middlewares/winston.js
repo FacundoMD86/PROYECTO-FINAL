af76b1519759bb49722538faf075e28ff8f34544
FacundoMD86 <124894865+FacundoMD86@users.noreply.github.com>
2024-01-08 01:17:10 -0300

    Delete src directory

--- src/middlewares/winston.js
+++
@@ -1,9 +0,0 @@
-import loggerDev from "../config/loggers/loggerDev.js";
-
-export default (req, res, next) => {
-  req.logger = loggerDev;
-  req.logger.HTTP(
-    `${req.method} ${req.url} - ${new Date().toLocaleTimeString()} - ${process.pid}`
-  );
-  return next();
-};
